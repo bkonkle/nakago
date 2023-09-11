a61fd7946b8d3704b08f310e025628e1e85f5c2d
Brandon Konkle <brandon@konkle.us>
2023-09-11 15:45:38 -0600

    Update path to documentation site

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -10,8 +10,8 @@ const config = {
     tagline: "A lightweight Rust framework for sharp services",
     favicon: "img/favicon.ico",
 
-    url: "https://bkonkle.github.io",
-    baseUrl: "/nakago/",
+    url: "https://nakago.dev",
+    baseUrl: "/",
 
     organizationName: "bkonkle",
     projectName: "nakago",
