c10d3e92ae072e0a1f702b0679f28576a88081e5
Brandon Konkle <brandon@konkle.us>
2023-10-25 18:50:04 -0600

    Improve the test coverage a bit

--- nakago/src/inject/hooks.rs
+++ nakago/src/inject/hooks.rs
@@ -20,3 +20,43 @@ impl Inject {
         hook.handle(self.clone()).await
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use fake::Fake;
+
+    use super::*;
+    use crate::{
+        inject::{self, container::test::TestService, tag::test::SERVICE_TAG, Key},
+        Inject,
+    };
+
+    #[derive(Debug)]
+    struct TestHook;
+
+    #[async_trait]
+    impl Hook for TestHook {
+        async fn handle(&self, i: Inject) -> inject::Result<()> {
+            i.inject(&SERVICE_TAG, TestService::new(fake::uuid::UUIDv4.fake()))
+                .await?;
+
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_hook() {
+        let i = Inject::default();
+
+        let hook = TestHook;
+
+        i.handle(hook).await.unwrap();
+
+        assert!(
+            i.0.read()
+                .await
+                .contains_key(&Key::from_tag::<TestService>(&SERVICE_TAG)),
+            "key does not exist in injection container"
+        );
+    }
+}
